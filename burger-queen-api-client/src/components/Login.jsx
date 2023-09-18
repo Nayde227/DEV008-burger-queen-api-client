3c831bf8ce9c375751684f31757214e0ca4d99b4
Nayde227 <naylimar27@gmail.com>
2023-09-18 13:20:50 -0300

    local storage intento 1

--- burger-queen-api-client/src/components/Login.jsx
+++ burger-queen-api-client/src/components/Login.jsx
@@ -1,15 +1,50 @@
+import { useState } from "react";
+import axios from "axios";
 export default function Login() {
-  
+
+   const [email, setEmail] = useState('')
+   const [password, setPassword] = useState('')
     
+   const payload = {
+    emailId: email,
+    password: password,
+    };
+
+    const onSubmitHandler = () => {
+      axios
+      .post(
+      "PASTE YOUR API HERE",
+      payload
+      )
+      .then((response) => {
+      const response1 = response;
+      localStorage.setItem("stringify", JSON.stringify(response1));
+      localStorage.setItem("Mytoken", response1.data.token);
+      });
+      };
+
   return (
         <div className="padreLogin">
           <h1>Burger Queen App</h1>
           <form className="formulario">
-          <input className='Email' type="email" placeholder='EMAIL'></input>
+          <input 
+           className='Email'
+           type="email" 
+           onChange={(e) => setEmail(e.target.value)} 
+           placeholder='EMAIL'>
+           </input>
           <br></br>
-          <input className='Password' type="password" placeholder='PASSWORD'></input>
+          <input 
+          className='Password' 
+          type="password" 
+          placeholder='PASSWORD'
+          onChange={(e) => setPassword(e.target.value)}>
+          </input>
           <br></br>
-          <button className='singIn' type="submit">Sing In</button>
+          <button 
+          className='singIn' 
+          type="submit"
+          onClick={onSubmitHandler}>Sing In</button>
           </form>
         </div>
       );
