a4ae124137b52e9b4fb71604cee032765e9307a5
Nayde227 <naylimar27@gmail.com>
2023-09-21 20:02:27 -0300

    dropdown prueba

--- burger-queen-api-client/src/components/Waiters.jsx
+++ burger-queen-api-client/src/components/Waiters.jsx
@@ -4,7 +4,17 @@ export default function Waiters() {
         <div className="padreLogin">
 
             <h1>Burger Queen Waiters</h1>
-            
+            <h2>Create Order</h2>
+            {/* <input
+            list="products"
+            id="inputProducts"
+            placeholder="Add Products"
+          /> */}
+          <select class="Add Products">
+            <option value="Sandwich">Sandwich</option>
+            <option value="Orange Juice">Orange Juice</option>
+            <option value="Latte">Latte</option>
+          </select>
         </div>
     ) 
 }
